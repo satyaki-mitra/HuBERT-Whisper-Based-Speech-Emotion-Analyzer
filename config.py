01ba17be9c8b17922edd4840eee8ff8b2544caff
Satyaki Mitra <32154355+satyaki-mitra@users.noreply.github.com>
2025-07-25 18:44:14 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+# CONFIGURATION VARIABLES
+
+# DATA SAVER PATHS
+UPLOAD_FOLDER       = 'data/app_data/uploaded'
+RECORD_FOLDER       = 'data/app_data/recorded'
+SEGMENTATION_FOLDER = 'data/app_data/segmented'
+
+# MODEL NAMES AND SPECIFICATIONS
+HUBERT_MODEL_PATH   = 'local_model_files/hubert/'
+WHISPER_MODEL_PATH  = 'local_model_files/whisper/large-v3.pt'
+
+# AUTHENTICATION 
+HUGGINGFACE_TOKEN   = "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"
+
+# APP SPECIFICATIONS
+PORT                = 2024
+HOST                = 'localhost'
+
